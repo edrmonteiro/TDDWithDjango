be7a6057451ec484f2a77c19d21e34d4335da542
eduardo <eduardormonteiro@gmail.com>
2021-01-19 13:34:24 -0300

    create unit test to check view parameters

--- lists/tests.py
+++ lists/tests.py
@@ -4,4 +4,7 @@ from django.test import TestCase
 
 class HomePageTest(TestCase):
     def test_math_works(self):
-        self.assertEqual(1 + 1, 3)
+        response = self.client.get('/')
+        self.assertTrue(response.content.decode().startswith('<html>'))
+        self.assertIn ('<title>To-Do Lists</title>', response.content.decode())
+        self.assertTrue(response.content.decode().endswith('</html>'))
